ad81e95f08e58e35411f658a01a09730ec6a6d9e
git123 <1065767471@qq.com>
2019-05-04 17:20:15 +0800

    添加了一个HelloUser类

---
+++ HelloStruts1/src/main/java/com/itheima/web/action/UserAction/HelloUser.java
@@ -0,0 +1,10 @@
+package com.itheima.web.action.UserAction;  
+/**  
+ * ClassName:HelloUser <br/>  
+ * Function:  <br/>  
+ * Date:     2019年5月4日 下午5:19:00 <br/>       
+ */
+public class HelloUser {
+
+}
+  
